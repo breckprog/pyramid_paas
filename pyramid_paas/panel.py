8720010f528198796a45dd4747e0c93bb0213048
Niall O'Higgins <niallo@unworkable.org>
2012-08-10 14:05:36 -0600

    update to generic "paas debug panel" rather than being dotcloud specific

--- pyramid_paas/panel.py
+++ pyramid_paas/panel.py
@@ -5,26 +5,26 @@ from pyramid_debugtoolbar.panels import DebugPanel
 _ = lambda x: x
 
 
-class DotCloudDebugPanel(DebugPanel):
+class PaaSDebugPanel(DebugPanel):
     """
-    DotCloud debug panel
+    PaaS debug panel
     """
-    name = 'DotCloud'
+    name = 'PaaS'
     has_content = True
 
     def nav_title(self):
-        return _('DotCloud')
+        return _('PaaS')
 
     def url(self):
         return ''
 
     def title(self):
-        return _('DotCloud')
+        return _('PaaS')
 
     def content(self):
-        env = [(k, v) for k, v in vars(self.request.dotcloud_env).iteritems()]
+        env = [(k, v) for k, v in vars(self.request.paas_env).iteritems()]
         return self.render(
-            'pyramid_paas:dotcloud.dbtmako',
+            'pyramid_paas:paas.dbtmako',
             {'env': sorted(env, key=itemgetter(0))},
             self.request
             )
@@ -33,6 +33,6 @@ class DotCloudDebugPanel(DebugPanel):
 def includeme(config):
     settings = config.registry.settings
     if 'debugtoolbar.panels' in settings:
-        settings['debugtoolbar.panels'].append(DotCloudDebugPanel)
+        settings['debugtoolbar.panels'].append(PaaSDebugPanel)
         if not 'mako.directories' in settings:
             settings['mako.directories'] = []
